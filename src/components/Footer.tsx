fa95fa970383d6fe7fb13c3dbb8dd1efcc893c4d
Roman S <60362318+rshushin@users.noreply.github.com>
2025-05-13 23:40:19 +0300

    Create Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => (
+  <footer className="text-center text-sm mt-10 text-gray-500">
+    <a href="/terms.html"   className="underline mr-4">Условия&nbsp;использования</a>
+    <a href="/privacy.html" className="underline mr-4">Политика&nbsp;конфиденциальности</a>
+    <a href="/refund.html"  className="underline">Политика&nbsp;возврата</a>
+  </footer>
+);
+
+export default Footer;
